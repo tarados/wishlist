aad7e528967fede33f874e381b3c363e079f1da1
sergey <lith2009@mail.ru>
2017-09-08 05:39:04 +0300

    2

--- wishapp/views.py
+++ wishapp/views.py
@@ -6,6 +6,6 @@ import datetime
 def desire (request):
     name = request.POST.get('name', '')
     desires = request.POST.get('desire', '')
-
+    publication_date = datetime.datetime.now()
     a = name + ' ' + desires
     return render(request, 'index.html', locals())

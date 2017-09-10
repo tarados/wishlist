411488516d2f2c8f93fd90c1eb7bc92536f41ed8
sergey <lith2009@mail.ru>
2017-09-10 22:15:13 +0300

    3

--- wishapp/models.py
+++ wishapp/models.py
@@ -1,15 +1,9 @@
 from django.db import models
 from django.contrib import admin
 
-# class User(models.Model):
-#     first_name = models.CharField(max_length=30)
-#     def __unicode__(self):
-#         return u'%s %s' % (self.first_name, self.last_name)
-
 class Wishmaker(models.Model):
     user = models.CharField(max_length=40)
     description = models.TextField()
-    avatar = models.ImageField()
     publication_date = models.DateField()
 
     def __unicode__(self):
--- wishapp/views.py
+++ wishapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from wishapp.models import Wishmaker
 import datetime
 
 def desire (request):
     name = request.POST.get('name', '')
     desires = request.POST.get('desire', '')
-    publication_date = datetime.datetime.now()
-    a = name + ' ' + desires
+    if name:
+        record = Wishmaker.objects.create(user=name, description=desires, publication_date=datetime.datetime.now())
+        record.save()
+    a = Wishmaker.objects.all()
     return render(request, 'index.html', locals())

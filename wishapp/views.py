9d28a316cad0d5cfced589bda141057b3e95523d
sergey <lith2009@mail.ru>
2017-09-30 22:46:16 +0300

    24

--- wishapp/views.py
+++ wishapp/views.py
@@ -17,16 +17,16 @@ def dreamers (request):
 
 def dreamer (request, dreamer_id):
     desire_form = DesireForm
-    if auth.get_user(request).id:
-        arg = {}
-        arg.update(csrf(request))
-        arg['dreamer'] = User.objects.get(id=dreamer_id)
-        arg['desires'] = Desire.objects.filter(desire_user_id=dreamer_id)
-        arg['form'] = desire_form
-        arg['username'] = auth.get_user(request).username
+    arg = {}
+    arg.update(csrf(request))
+    arg['dreamer'] = User.objects.get(id=dreamer_id)
+    arg['desires'] = Desire.objects.filter(desire_user_id=dreamer_id)
+    arg['form'] = desire_form
+    arg['username'] = auth.get_user(request).username
+    if auth.get_user(request).id == int(dreamer_id):
         return render_to_response('dreamer.html', arg)
     else:
-        return redirect('/')
+        return redirect('/auth/logout/')
 
 
 def adddesire(request, dreamer_id):

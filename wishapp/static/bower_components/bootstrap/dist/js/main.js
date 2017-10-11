02d564bcd32acbf18438fc45c3011d9703be6716
Sergey <lith2009@mail.ru>
2017-10-11 11:53:15 +0300

    38

--- wishapp/static/bower_components/bootstrap/dist/js/main.js
+++ wishapp/static/bower_components/bootstrap/dist/js/main.js
@@ -7,8 +7,9 @@ $(function () {
         var form = $("#desireform-" + desireid);
         var strold = form.text().substring(form.text().indexOf("<a href"), form.text().indexOf("</a>") + 4);
         var strnew = form.text().substring(form.text().indexOf(">http") +1, form.text().indexOf("</a>"));
-        var  str = form.text().replace(strold, strnew);
-         this.form.text() = form.text(str));
+        var newform = form.text().replace(strold, strnew);
+        var textarea = $("form textarea");
+        var str = textarea.text(newform);
         info.addClass("hidden");
         form.removeClass("hidden");
         console.log(str);

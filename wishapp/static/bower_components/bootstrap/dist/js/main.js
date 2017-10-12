49610b36e4dc48726675ffc6a4ef6561b156d882
sergey <lith2009@mail.ru>
2017-10-13 00:43:44 +0300

    42

--- wishapp/static/bower_components/bootstrap/dist/js/main.js
+++ wishapp/static/bower_components/bootstrap/dist/js/main.js
@@ -5,13 +5,7 @@ $(function () {
         var desireid = id.split("-")[1];
         var info = $("#desireinfo-" + desireid);
         var form = $("#desireform-" + desireid);
-        // var strold = form.text().substring(form.text().indexOf("<a href"), form.text().indexOf("</a>") + 4);
-        // var strnew = form.text().substring(form.text().indexOf(">http") +1, form.text().indexOf("</a>"));
-        // var newform = form.text().replace(strold, strnew);
-        var textarea = $("textarea", form);
-        var str = textarea.text();
         info.addClass("hidden");
         form.removeClass("hidden");
-        console.log(str.indexOf(">http", 72));
     });
 });

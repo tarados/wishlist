dabb424efdad9fbc390fd1254f151ea26c069711
sergey <lith2009@mail.ru>
2017-10-14 10:11:38 +0300

    46

--- wishapp/linkcoder.py
+++ wishapp/linkcoder.py
@@ -1,14 +1,8 @@
 import re
 
-str = 'http://127.0.0.1:8000/dreamers/1/ вы https://metanit.com/web/javascript/6.1.php явааи' \
-      ' http://othervision.info/zayavleniya-rossii-o-sposobnosti-s-400-pobedit-f-35-okazalis-fejkom-the-national-interest/?utm_referrer=https%3A%2F%2Fzen.yandex.com '
-abc = 'http://127.0.0.1:8000/dreamers/1/ dlkfj https://webformyself.com/ fghjdfg https://www.8host.com/blog/cikly-for-v-python-3/ ytoouo '
-
 def linkOn(str):
     if re.search(r'[Hh-sS]\w+:[//.aA-zZ:\-?&=%\d+\w+]+', str):
         for c in re.findall(r'[Hh-sS]\w+:[//.aA-zZ:\-?&=%\d+\w+]+', str):
             b = '<a href="' + c + '">' + c + '</a>'
             str = str.replace(c, b)
     return str
-
-linkOn(str)

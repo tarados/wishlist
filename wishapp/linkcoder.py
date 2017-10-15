65214eb8236850eff37fac2592eb9c8a050d055f
sergey <lith2009@mail.ru>
2017-10-15 18:48:33 +0300

    50

--- wishapp/linkcoder.py
+++ wishapp/linkcoder.py
@@ -1,6 +1,6 @@
 import re
 
-
+str = 'test http://google.com sdsaasddds\n dfgsdgdbasdg http://google.com/?q=1\n http://google.com/?q=samsung\n dd http://google.com/?q=samsung'
 def linkOn(str):
     newstr = []
     str = str.split(' ')
@@ -8,9 +8,14 @@ def linkOn(str):
         if re.search(r'[Hh-sS]\w+:[//.aA-zZ:\-?&=%#\d+\w+]+', substr):
             c = re.search(r'[Hh-sS]\w+:[//.aA-zZ:\-?&=%#\d+\w+]+', substr).group()
             b = '<a href="' + c + '">' + c + '</a>'
-            newstr.append(b)
+            substr = substr.replace(c, b)
+            newstr.append(substr.replace('\n', '<br>'))
         else:
+            substr = substr.replace('\n', '<br>')
             newstr.append(substr)
     str = ' '.join(newstr)
+    # if re.search(r'\n', str):
+   # str = str.replace('\n', '<br>')
     return str
 
+print(linkOn(str))

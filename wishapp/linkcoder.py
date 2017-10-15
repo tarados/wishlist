d0423b2ae0ed200d6f46c903995dbdbe682b8e19
sergey <lith2009@mail.ru>
2017-10-15 13:33:29 +0300

    47

--- wishapp/linkcoder.py
+++ wishapp/linkcoder.py
@@ -1,8 +1,15 @@
 import re
 
+a = 'test http://google.com sdsaasddds dfgsdgdbasdg http://google.com/?q=1 http://google.com/?q=samsung dd http://google.com/?q=samsung'
+d = 'test http://google.com/?q=1 sdsaasddds dfgsdgdbasdg http://google.com http://google.com/?q=samsung dd http://google.com/?q=samsung'
+
 def linkOn(str):
     if re.search(r'[Hh-sS]\w+:[//.aA-zZ:\-?&=%\d+\w+]+', str):
         for c in re.findall(r'[Hh-sS]\w+:[//.aA-zZ:\-?&=%\d+\w+]+', str):
+            print(c)
             b = '<a href="' + c + '">' + c + '</a>'
+            print(b)
             str = str.replace(c, b)
     return str
+
+print(linkOn(d))

722822452eef53ebf0c1b57dee7b964de62fd212
sergey <lith2009@mail.ru>
2017-11-13 06:54:07 +0300

    на работу

--- wishapp/static/bower_components/bootstrap/dist/js/main.js
+++ wishapp/static/bower_components/bootstrap/dist/js/main.js
@@ -8,16 +8,10 @@ $(function () {
         info.addClass("hidden");
         form.removeClass("hidden");
     });
-    // $(".select_desire").click(function () {
-    //     var button = $(this);
-    //     var id = button.attr("id");
-    //     var desire_select_id = id.split("-")[1];
-    //     var desire_select_label_id = id.split("-")[1];
-    //     var info1 = $("#desireselect-" + desire_select_id);
-    //     var label_select =$("#labelselect-" + desire_select_label_id);
-    //     info1.addClass("hidden");
-    //     label_select.removeClass("hidden");
-    // });
+
+    console.log($("div.st").length);
+
+
     var info2 = $("div.ordered");
     info2.css('background', 'red');
     links = $("div.well a");

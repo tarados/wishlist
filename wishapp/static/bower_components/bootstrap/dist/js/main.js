bcdf6272871f9dbf76e0dd7eae8b170bbd95685a
sergey <lith2009@mail.ru>
2017-12-16 16:51:31 +0300

    Shape Hover Effect demo2

--- wishapp/static/bower_components/bootstrap/dist/js/main.js
+++ wishapp/static/bower_components/bootstrap/dist/js/main.js
@@ -66,6 +66,44 @@ $(function () {
         $("html, body").animate({scrollTop: 0}, 10);
         return false;
     });
+// aaaaaaaaaaaaaaaaaaaaaaaaaaa
+//     var _gaq = _gaq || [];
+//     _gaq.push(['_setAccount', 'UA-7243260-2']);
+//     _gaq.push(['_trackPageview']);
+//     (function() {
+//     var ga = document.createElement('script');
+//     ga.type = 'text/javascript';
+//     ga.async = true;
+//     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+//     var s = document.getElementsByTagName('script')[0];
+//     s.parentNode.insertBefore(ga, s);
+//     })();
 
+// // gggggggggggggggggggggggggggggggggggggggg Shape Hover Effect
+    (function() {
+        function init(){
+            var speed = 330,
+                easing = mina.backout;
+
+            [].slice.call ( document.querySelectorAll( '#grid > a' ) ).forEach( function( el ) {
+                var s = Snap( el.querySelector( 'svg' ) ), path = s.select( 'path' ),
+                    pathConfig = {
+                    from : path.attr( 'd' ),
+                        to : el.getAttribute( 'data-path-hover' )
+                        };
+
+                el.addEventListener( 'mouseenter', function() {
+                    path.animate( { 'path' : pathConfig.to }, speed, easing );
+                    });
+
+                el.addEventListener( 'mouseleave', function() {
+                    path.animate( { 'path' : pathConfig.from }, speed, easing );
+                    });
+                });
+            }
+
+            init();
+
+        })();
 });
 

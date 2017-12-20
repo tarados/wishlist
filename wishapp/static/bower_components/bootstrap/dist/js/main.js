8eafcc57ca075f75dc3eebd8d566b33bc01b2dff
Sergey <lith2009@mail.ru>
2017-12-20 15:53:45 +0200

    анимация - ок

--- wishapp/static/bower_components/bootstrap/dist/js/main.js
+++ wishapp/static/bower_components/bootstrap/dist/js/main.js
@@ -11,22 +11,22 @@ $(function () {
     });
     // убираем блок с кнопками у выбранного желания и выделяем цветом
     var states = $("li.button");
-    for (i=0; i<states.length; i++){
-         state = states[i].getAttribute("class");
-         var c = state.split("-");
-         if (c[5] == "False") {
-             if (c[3] == 1) {
-                 states[i].setAttribute("class", "hidden");
-                 var dt = $("li.dt")[i];
-                 dt.style.backgroundColor = "coral";
-             }
+    for (i = 0; i < states.length; i++) {
+        state = states[i].getAttribute("class");
+        var c = state.split("-");
+        if (c[5] == "False") {
+            if (c[3] == 1) {
+                states[i].setAttribute("class", "hidden");
+                var dt = $("li.dt")[i];
+                dt.style.backgroundColor = "coral";
             }
+        }
     }
-         // ссылки в желаниях открываем в другом окне
+    // ссылки в желаниях открываем в другом окне
     var info2 = $("div.ordered");
     info2.css('background', 'red');
     links = $("li.tx a");
-    for (i=0; i<links.length; i++){
+    for (i = 0; i < links.length; i++) {
         link = links[i];
         link.target = "_blank";
     }
@@ -37,23 +37,25 @@ $(function () {
         box_error.removeClass("hidden");
     }
     // модуль перетаскивания мышкой желаний
-    Sortable.create(simpleList, {onEnd: function (evt) {
-        var desire_order_list = new Array();
-        var desire_order = new Object();
-        var desireinfo = $(".drs").each(function (index) {
-            var id = $(this).attr("id");
-            var desirecounter = id.split("-")[3];
-            var desire_id = id.split("-")[1];
-            $(".loopcounter")[index].innerHTML = index + 1;
-            desire_order = {
-                "desire_id": desire_id,
-                "loopcounter": $(".loopcounter")[index].innerHTML
-            };
-            desire_order_list.push(desire_order);
-        });
-         list_for_save = JSON.stringify(desire_order_list);
-        $.post('/order/', {"a": list_for_save});
-    }});
+    Sortable.create(simpleList, {
+        onEnd: function (evt) {
+            var desire_order_list = new Array();
+            var desire_order = new Object();
+            var desireinfo = $(".drs").each(function (index) {
+                var id = $(this).attr("id");
+                var desirecounter = id.split("-")[3];
+                var desire_id = id.split("-")[1];
+                $(".loopcounter")[index].innerHTML = index + 1;
+                desire_order = {
+                    "desire_id": desire_id,
+                    "loopcounter": $(".loopcounter")[index].innerHTML
+                };
+                desire_order_list.push(desire_order);
+            });
+            list_for_save = JSON.stringify(desire_order_list);
+            $.post('/order/', {"a": list_for_save});
+        }
+    });
     // кнопка "наверх"
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
@@ -66,43 +68,27 @@ $(function () {
         $("html, body").animate({scrollTop: 0}, 10);
         return false;
     });
-// // gggggggggggggggggggggggggggggggggggggggg Shape Hover Effect
-    (function() {
-        function init(){
-            var speed = 330,
-                easing = mina.backout;
+    // анимация
+    function init() {
+        var speed = 250,
+            easing = mina.easeinout;
 
-            [].slice.call ( document.querySelectorAll( '#grid > a' ) ).forEach( function( el ) {
-                var s = Snap( el.querySelector( 'svg' ) ), path = s.select( 'path' ),
-                    pathConfig = {
+        [].slice.call ( document.querySelectorAll( '#grid > a' ) ).forEach( function( el ) {
+            var s = Snap( el.querySelector( 'svg' ) ), path = s.select( 'path' ),
+                pathConfig = {
                     from : path.attr( 'd' ),
-                        to : el.getAttribute( 'data-path-hover' )
-                        };
+                    to : el.getAttribute( 'data-path-hover' )
+                };
 
-                el.addEventListener( 'mouseenter', function() {
-                    path.animate( { 'path' : pathConfig.to }, speed, easing );
-                    });
+            el.addEventListener( 'mouseenter', function() {
+                path.animate( { 'path' : pathConfig.to }, speed, easing );
+            } );
 
-                el.addEventListener( 'mouseleave', function() {
-                    path.animate( { 'path' : pathConfig.from }, speed, easing );
-                    });
-                });
-            }
+            el.addEventListener( 'mouseleave', function() {
+                path.animate( { 'path' : pathConfig.from }, speed, easing );
+            } );
+        } );
+    }
 
-            init();
-
-        })();
-    // отрабатываем нажатие кнопки "view"
-      $("a.des").click(function () {
-          var link = $(this);
-          var id = link.attr('id');
-          var info = $('#' + id);
-          $('div .list-group-item').each(function () {
-              // console.log(this.id);
-          })
-          info.addClass("hidden");
-          // form.removeClass("hidden");
-          console.log($('div .list-group-item')[0]);
-      });
+    init();
 });
-

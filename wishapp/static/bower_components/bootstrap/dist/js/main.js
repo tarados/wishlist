95b6eca8cca6cbec65aadd3bb112303413d831b6
Sergey <lith2009@mail.ru>
2018-01-17 08:20:46 +0200

    елочка при перетаскивании - ок!

--- wishapp/static/bower_components/bootstrap/dist/js/main.js
+++ wishapp/static/bower_components/bootstrap/dist/js/main.js
@@ -112,8 +112,13 @@ $(function () {
             });
             list_for_save = JSON.stringify(desire_order_list);
             $.post('/order/', {"a": list_for_save});
+            $("div.grid-link").removeClass('even');
+            $("div.grid-link").removeClass('odd');
+            $("div.grid-link:even").addClass('even');
+            $("div.grid-link:odd").addClass('odd');
+
         }
     });
-    // $(".al:even").css("margin", "30px 6px -23px");
+
 });
 
